91c929deb37b5cc138ed23f39521117d09e0176b
David Howell <davidh@dynamicmethods.com.au>
2019-10-28 14:07:02 +1030

    Added basic stdout option

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var pump = require('pump');
 var ChunkyStream = require('chunky-stream');
-var CloudWatchStream = require('./lib/cloudwatch-stream');
+var StdoutStream = require('./lib/stdout-stream');
 var ThrottleStream = require('./lib/throttle-stream');
+var CloudWatchStream = require('./lib/cloudwatch-stream');
 
 module.exports = function (options) {
   var log = new CloudWatchStream(options);
   var chunk = new ChunkyStream(options);
   var throttle = new ThrottleStream();
+  var stdout = new StdoutStream(options);
 
   chunk.use(require('./lib/max-length'));
   chunk.use(require('./lib/max-size'));
 
-  chunk.pipe(throttle).pipe(log);
+  stdout.pipe(chunk).pipe(throttle).pipe(log);
 
-  return chunk;
+  return stdout;
 };
---
+++ test/specs/stdout-stream.spec.js
@@ -0,0 +1,43 @@
+require('chai').should();
+var mockRequire = require('mock-require');
+mockRequire('aws-sdk', require('../mocks/aws-sdk'));
+
+var fs = require('fs');
+var path = require('path');
+var pump = require('pump');
+var split = require('split2');
+var StdoutStream = require('../../lib/stdout-stream');
+
+describe('stdout-stream', function () {  
+  it('should copy input to stdout', function (done) {
+    var didLog = false;
+    var inStream = fs.createReadStream(path.resolve(__dirname, '../mocks/logs.txt'));
+    var mockConsole = {
+      log: function (message) {
+        didLog = true;
+      }
+    };
+    var stream = new StdoutStream({ stdout: true, console: mockConsole });
+
+    pump(inStream, split(), stream, function (err) {
+      didLog.should.be.true;
+      done(err);
+    });
+  });
+
+  it('should not copy input to stdout', function (done) {
+    var didLog = false;
+    var mockConsole = {
+      log: function (message) {
+        didLog = true;
+      }
+    };
+    var stream = new StdoutStream({ stdout: false, console: mockConsole });
+    var inStream = fs.createReadStream(path.resolve(__dirname, '../mocks/logs.txt'));
+
+    pump(inStream, split(), stream, function (err) {
+      didLog.should.be.false;
+      done(err);
+    });
+  });
+});
